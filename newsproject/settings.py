d470932f79ecf766a08e8713007d963c0af8370e
d-rooX <bot0463@gmail.com>
2021-11-10 04:28:09 +0800

    ...

--- newsproject/settings.py
+++ newsproject/settings.py
@@ -125,6 +125,10 @@ USE_TZ = True
 
 STATIC_URL = "/static/"
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+MEDIA_URL = "/media/"
+MEDIA_ROOT = os.path.join(BASE_DIR, "media")
+
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 LOGIN_URL = "/login"
